3f19a5f70c379a12f98267b60cb0291fc53512ea
Roman Proshunin <proshuninreal@gmail.com>
2022-11-02 13:04:38 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -12,14 +12,7 @@ const form = {
 
 var cbElements = document.getElementById("checkbox1");
 
-let checked = false
 
-cbElements.addEventListener('click', function() {
-  this.getElementsByClassName("check")[0].classList.toggle('active');
-  this.getElementsByClassName("check-text")[0].classList.toggle('active');
-  checked = !checked
-  checkForm()
-})
 
 function checkForm() {
   var cbElements = document.getElementsByClassName("checkbox1");
@@ -44,38 +37,9 @@ function easyInput(event, name) {
   checkForm();
 }
 
-let entryLog = true;
-let entryPass = true;
 
-function validateLogin() {
-  const  value  = form.Login.getElementsByTagName("input")[0];
-  if (value.value.length >= 3) {
-    form.LogErr.classList.remove("view");
-    form.Login.classList.remove("error");
-    entryLog = true
-  } else {
-    form.Button.classList.add("disable");
-    form.Login.classList.remove("filled");
-    form.Login.classList.add("error");
-    form.LogErr.classList.add("view");
-    entryLog = false
-  }
-  const  value2 = form.Pass.getElementsByTagName("input")[0];
-  if (value2.value.length >= 3) {
-    form.PassErr.classList.remove("view");
-    form.Pass.classList.remove("error");
-    entryPass = true
-  } else {
-    form.Button.classList.add("disable");
-    form.Pass.classList.remove("filled");
-    form.Pass.classList.add("error");
-    form.PassErr.classList.add("view");
-    entryPass = false
-  }
-  if (entryLog && entryPass) {
-    alert('okay')
-  }
-}
+
+
 
 form.Login.oninput = (event) => easyInput(event, "Login");
 form.Pass.oninput = (event) => easyInput(event, "Pass");
